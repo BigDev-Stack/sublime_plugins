14379cc85668d0ff43eecd472cba357c21624a05
BigDev <1404974345@qq.com>
2022-05-07 17:12:04 +0800

    update cxx plugins

--- Cxx/Cxx.py
+++ Cxx/Cxx.py
@@ -19,44 +19,57 @@ def parseClassName(clazz):
     return True
 
 
+def createCxxTemplate(path, isCpp):
+    if not os.path.exists(path) or os.path.isfile(path):
+        print('invalid path:', path)
+        return
+
+    def on_done(usrInput):
+        print('user input:', usrInput)
+        print('create cxx files at', path)
+        usrInput = usrInput.strip(' ')
+        if not parseClassName(usrInput):
+            print('invalid class name')
+            return
+        fileName = ''
+        for ch in usrInput:
+            if ch >= 'A' and ch <= 'Z':
+                if fileName:
+                    fileName += '_'
+            fileName += ch.lower()
+        print('file name:', fileName)
+        prefix = os.path.join(path, fileName)
+        headerPath = prefix + '.h'
+        srcPath = prefix + ('.cc' if isCpp else '.c')
+        if not os.path.exists(headerPath):
+            with open(headerPath, 'w') as file:
+                file.write('#pragma once\n\n')
+                if isCpp:
+                    file.write('class {} {};'.format(usrInput, '{}'))
+        if not os.path.exists(srcPath):
+            with open(srcPath, 'w') as file:
+                file.write('#include "{}"\n'.format(fileName + '.h'))
+
+    def on_cancel():
+        print('cancel')
+
+    window = sublime.active_window()
+    window.show_input_panel("cxx name", "", on_done, None, on_cancel)
+
+
 class CxxCommand(sublime_plugin.TextCommand):
 
     def run(self, edit, paths=None):
         if not paths:
             print('no path specified')
             return
-        path = paths[0]
-        if not os.path.exists(path) or os.path.isfile(path):
-            print('invalid path:', path)
+        createCxxTemplate(paths[0], True)
+
+
+class CCommand(sublime_plugin.TextCommand):
+
+    def run(self, edit, paths=None):
+        if not paths:
+            print('no path specified')
             return
-
-        def on_done(usrInput):
-            print('user input:', usrInput)
-            print('create cxx files at', path)
-            usrInput = usrInput.strip(' ')
-            if not parseClassName(usrInput):
-                print('invalid class name')
-                return
-            fileName = ''
-            for ch in usrInput:
-                if ch >= 'A' and ch <= 'Z':
-                    if fileName:
-                        fileName += '_'
-                fileName += ch.lower()
-            print('file name:', fileName)
-            prefix = os.path.join(path, fileName)
-            headerPath = prefix + '.h'
-            srcPath = prefix + '.cc'
-            if not os.path.exists(headerPath):
-                with open(headerPath, 'w') as file:
-                    file.write('#pragma once\n\n')
-                    file.write('class {} {};'.format(usrInput, '{}'))
-            if not os.path.exists(srcPath):
-                with open(srcPath, 'w') as file:
-                    file.write('#include "{}"\n'.format(fileName + '.h'))
-
-        def on_cancel():
-            print('cancel')
-
-        window = sublime.active_window()
-        window.show_input_panel("cxx name", "", on_done, None, on_cancel)
+        createCxxTemplate(paths[0], False)
